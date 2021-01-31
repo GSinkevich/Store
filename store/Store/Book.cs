b507fdf2ef41c3fa5539dcd991e6659e58c7b4c4
Sinkevich <sinkevich.work@gmail.com>
2021-01-31 17:28:24 +0300

    #1 Init Solution and add Store/Book

---
+++ store/Store/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Store
+{
+    public class Book
+    {
+        public int Id { get; }
+        public string Title { get; } //property 
+
+        public Book (int id, string title)
+        {
+            Id = id;
+            Title = title;
+        }
+
+    }
+}
